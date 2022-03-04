c710acbc8c9c9a79196662cc374693e87aa4ee75
vickyruud <aprajit.k@gmail.com>
2022-03-04 10:10:42 -0500

    create chat feed components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { ChatEngine } from "react-chat-engine";
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
 
 const App = () => {
   return (
     <ChatEngine
       height='100vh'
-      projectID=''
-      userName=''
-      userSecret=''
+      projectID='98444411-ce63-4144-a874-31fd5d4743f8'
+      userName='captslow'
+      userSecret='123456'
+      renderChatFeed={(chatAppProps) =><ChatFeed { ...chatAppProps } /> } 
     />
   );
 }
